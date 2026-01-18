fe26241a19b83e83c47b313a7718159645e2be8b
Edwin Salvador Soto Hernandez <edwinsotohz@gmail.com>
2026-01-17 21:57:28 -0600

    comm: LoRa communication v2 (text)

--- LoRa-Communication-Test/LoRa-SX1278-Receiver/LoRa-SX1278-Receiver.ino
+++ LoRa-Communication-Test/LoRa-SX1278-Receiver/LoRa-SX1278-Receiver.ino
@@ -1,80 +1,61 @@
 #include <LoRa.h>
 
-// Pines RECEPTOR
+// pines RECEPTOR
 const int loraRST = 15;
-const int loraDI0 = 2;  // DIO0
+const int loraDI0 = 2;
 const int loraNSS = 5;
 const int loraMOSI = 18;
 const int loraMISO = 22;
 const int loraSCK = 23;
 
 int SyncWord = 0x22;
-int lastReceivedValue = 0; // (Solo para test)
-int currentValue = 0;
+String lastMessage = "";
 
 void setup() {
   Serial.begin(115200);
   
   while (!Serial);
-  Serial.println("Esperando datos...");
-
+  
   // SPI
   SPI.begin(loraSCK, loraMISO, loraMOSI, loraNSS);
   
-  // pines para LoRa
+  // pines LoRa
   LoRa.setPins(loraNSS, loraRST, loraDI0);
 
-  // LoRa en 433MHz
+  // 433mhz
   if (!LoRa.begin(433E6)) {
-    Serial.println("Starting LoRa failed!");
+    Serial.println("Error de Inicializacion de LoRa");
     while (1);
   }
   
-  // Parámetros LoRa
+  // Configurar parámetros
   LoRa.setSpreadingFactor(12);
   LoRa.setSignalBandwidth(62.5E3);
   LoRa.setCodingRate4(8);
   LoRa.setSyncWord(SyncWord);
   
-  Serial.println("LoRa init succeeded.");
-  Serial.println();
+  Serial.println("Receptor listo");
+  Serial.println("Esperando mensaje...");
 }
 
 void loop() { 
-  // Recibir un paquete
   int packetSize = LoRa.parsePacket();
   
   if (packetSize) { 
     String receivedData = "";
     
-    // Leer paquete
     while (LoRa.available()) {
       char inChar = (char)LoRa.read();
       receivedData += inChar;
     }
     
-    // Convercion a número
-    currentValue = receivedData.toInt();
-    
-    // Para un valor nuevo
-    if (currentValue != lastReceivedValue) {
-      lastReceivedValue = currentValue;
-      
-      // Mostrar
-      Serial.print("Nuevo dato recibido: ");
-      Serial.println(currentValue);
-      
-      // Información de señal (test)
-      Serial.print("RSSI: ");
-      Serial.print(LoRa.packetRssi());
-      Serial.print(" dBm, SNR: ");
-      Serial.print(LoRa.packetSnr());
-      Serial.println(" dB");
-      Serial.println("-------------------");
+    if (receivedData != lastMessage) {
+      lastMessage = receivedData;
+      Serial.println(receivedData);
+      Serial.println("-----------------");
       Serial.println();
-      Serial.println("Esperando siguiente dato...");
     }
   }
   
-  delay(50); // Pequeño delay para evitar sobrecarga
+  delay(50);
 }
--- LoRa-Communication-Test/LoRa_SX1278_Sender/LoRa_SX1278_Sender.ino
+++ LoRa-Communication-Test/LoRa_SX1278_Sender/LoRa_SX1278_Sender.ino
@@ -1,68 +1,62 @@
 #include <LoRa.h>
 
-// Pines TRANSMISOR
+// pines TRANSMISOR
 const int loraRST = 23;
-const int loraDI0 = 7;  // DIO0
+const int loraDI0 = 7;
 const int loraNSS = 15;
 const int loraMOSI = 32;
 const int loraMISO = 35;
 const int loraSCK = 33;
 
 int SyncWord = 0x22;
-int dataToSend = 0;  // Solo para test (alamacenar)
+String lastMessage = "";
 
 void setup() {
   Serial.begin(115200);
   
   while (!Serial);
-  Serial.println("Ingresa un numero para enviar:");
-
+  
   // SPI
   SPI.begin(loraSCK, loraMISO, loraMOSI, loraNSS);
   
-  // Configurar pines LoRa
+  // pines LoRa
   LoRa.setPins(loraNSS, loraRST, loraDI0);
 
-  // LoRa en 433MHz
+  // 433mhz
   if (!LoRa.begin(433E6)) {
-    Serial.println("Starting LoRa failed!");
+    Serial.println("Error de Inicializacion de LoRa");
     while (1);
   }
 
-  // Parámetros LoRa
+  // Configurar parámetros
   LoRa.setSpreadingFactor(12);
   LoRa.setSignalBandwidth(62.5E3);
   LoRa.setCodingRate4(8);
   LoRa.setSyncWord(SyncWord);
 
-  Serial.println("LoRa init succeeded.");
-  Serial.println();
+  Serial.println("Transmisor listo");
+  Serial.println("Ingresar valor:");
 }
 
 void loop() {
-  // Leer monitor serial
   if (Serial.available() > 0) {
     String input = Serial.readStringUntil('\n');
     input.trim();
     
-    // Convertir numeros
     if (input.length() > 0) {
-      dataToSend = input.toInt();
+      lastMessage = input;
       
-      // Enviar el dato por LoRa
-      Serial.print("Enviando: ");
-      Serial.println(dataToSend);
-      
+      // Enviar por LoRa
       LoRa.beginPacket();
-      LoRa.print(dataToSend);
+      LoRa.print(input);
       LoRa.endPacket();
       
-      Serial.println("Exitoso!");
-      Serial.println("---------------------------------");
+      Serial.print("Enviado: ");
+      Serial.println(input);
+      Serial.println("-----------------");
       Serial.println();
-      Serial.println("Ingresa otro numero para enviar:");
     }
   }
   
-  delay(100); // evitar sobrecarga
+  delay(50);
 }
